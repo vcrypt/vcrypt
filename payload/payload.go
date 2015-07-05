051593acbb45c8639369eee92aa66af9bf043a0a
Ben Burkert <ben@benburkert.com>
2015-07-05 08:30:39 -0700

    payload: add Payload interface
    A Payload encrypts and optionally stores data for a vault.

---
+++ payload/payload.go
@@ -0,0 +1,63 @@
+package payload
+
+import (
+	"errors"
+	"io"
+
+	"github.com/benburkert/vcrypt/material"
+)
+
+// Payload encrypts and stores the data for a vault.
+type Payload interface {
+	// Digest is a unique series of bytes that identify the Payload.
+	Digest() ([]byte, error)
+
+	// Lock encrypts the data from the Reader and returns ciphertext.
+	Lock(io.Reader, material.DB) ([]byte, error)
+
+	// Unlock decrypts the ciphertext and writes the data to the Writer.
+	Unlock(io.Writer, []byte, material.DB) error
+
+	// Marshal returns the binary representation of the Payload.
+	Marshal() ([]byte, error)
+
+	// Unmarshal parses the Payload encoded in data.
+	Unmarshal([]byte) error
+}
+
+// Wrap returns an intermediate form of the Payload for marshalling.
+func Wrap(p Payload) (*Envelope, error) {
+	env := &Envelope{}
+	if !env.SetValue(p) {
+		return nil, errors.New("unknown Payload type")
+	}
+	return env, nil
+}
+
+// Payload returns the concrete type from the intermediate form.
+func (e *Envelope) Payload() (Payload, error) {
+	v := e.GetValue()
+	if v == nil {
+		return nil, errors.New("invalid Payload data")
+	}
+	return v.(Payload), nil
+}
+
+// Marshal returns the proto3 encoding of p.
+func Marshal(p Payload) ([]byte, error) {
+	env, err := Wrap(p)
+	if err != nil {
+		return nil, err
+	}
+	return env.Marshal()
+}
+
+// Unmarshal parses the proto3 encoded payload.
+func Unmarshal(data []byte) (Payload, error) {
+	env := &Envelope{}
+	if err := env.Unmarshal(data); err != nil {
+		return nil, err
+	}
+
+	return env.Payload()
+}
--- vcrypt.go
+++ vcrypt.go
@@ -5,6 +5,7 @@ import "github.com/benburkert/vcrypt/seal"
 //go:generate -command protoc protoc --proto_path=$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf:. --gogo_out=.
 //go:generate protoc cryptex/cryptex.proto cryptex/sss.proto cryptex/xor.proto cryptex/secretbox.proto cryptex/box.proto cryptex/rsa.proto cryptex/openpgp.proto cryptex/mux.proto cryptex/demux.proto
 //go:generate protoc material/material.proto
+//go:generate protoc payload/payload.proto
 //go:generate protoc seal/seal.proto seal/openpgp.proto
 //go:generate protoc secret/secret.proto secret/password.proto secret/openpgpkey.proto
 //go:generate protoc marker.proto node.proto
