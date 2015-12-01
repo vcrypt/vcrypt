3e2125b238b04f5314df330f037616b05a960328
Ben Burkert <ben@benburkert.com>
2015-11-30 19:20:28 -0500

    vcrypt: add Lock & Unlock methods to Vault
    The Lock method first encrypts data in a payload then secures the
    decryption key via the scheme described by the plan.
    
    The Unlock method unlocks the payload decryption key then decrypts the
    payload data.

--- vault.go
+++ vault.go
@@ -1,14 +1,19 @@
 package vcrypt
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha256"
 	"errors"
 	"io"
 
+	"github.com/benburkert/vcrypt/cryptex"
+	"github.com/benburkert/vcrypt/graph"
+	"github.com/benburkert/vcrypt/material"
 	"github.com/benburkert/vcrypt/payload"
 	"github.com/benburkert/vcrypt/seal"
+	"github.com/benburkert/vcrypt/secret"
 )
 
 // NewVault constructs a Vault from a Plan.
@@ -23,6 +28,88 @@ func NewVault(plan *Plan, comment string) (*Vault, error) {
 		Plan:    plan,
 		Nonce:   nonce,
 	}, nil
+}
+
+// Lock encrypts a vault by building an encrypted Payload from r. It then
+// secures the decryption key in a multi-step encryption scheme described in
+// the Plan.
+func (v *Vault) Lock(r io.Reader, drv Driver) error {
+	if v.payload != nil {
+		return errors.New("Vault already locked")
+	}
+
+	pld, rootKey, err := drv.LockPayload(r)
+	if err != nil {
+		return err
+	}
+
+	g, err := v.Plan.Graph()
+	if err != nil {
+		return err
+	}
+
+	walker := &vaultWalker{
+		graph:   g,
+		drv:     drv,
+		outputs: map[*graph.Vertex][][]byte{g.Root: [][]byte{rootKey}},
+	}
+
+	if err := g.BFS(walker.lock); err != nil {
+		return err
+	}
+
+	env, err := payload.Wrap(pld)
+	if err != nil {
+		return err
+	}
+
+	v.payload = env
+	v.Materials = walker.materials
+	return nil
+}
+
+// Unlock retrieves the Payload decryption key by solving the Plan and
+// writes the decrypted Payload data to w.
+func (v *Vault) Unlock(w io.Writer, drv Driver) (unlocked bool, err error) {
+	if v.payload == nil {
+		return false, errors.New("Vault is not locked")
+	}
+
+	g, err := v.Plan.Graph()
+	if err != nil {
+		return false, err
+	}
+
+	walker := &vaultWalker{
+		graph:     g,
+		drv:       drv,
+		materials: v.Materials,
+		outputs:   map[*graph.Vertex][][]byte{g.Root: [][]byte{nil}},
+		skipped:   map[*graph.Vertex]bool{},
+	}
+
+	if err := g.BFS(walker.shapeOutputs); err != nil {
+		return false, err
+	}
+
+	if err := g.ReverseDFS(walker.unlock); err != nil {
+		return false, err
+	}
+	if walker.skipped[g.Root] {
+		return false, nil
+	}
+	rootKey := walker.outputs[g.Root][0]
+
+	pld, err := v.Payload()
+	if err != nil {
+		return false, err
+	}
+
+	if err := pld.Unlock(w, rootKey, drv); err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
 
 // Comment string
@@ -100,4 +187,240 @@ func (v *Vault) Seals() ([]seal.Seal, error) {
 // Payload holds the encrypted data protected by the vault.
 func (v *Vault) Payload() (payload.Payload, error) {
 	return v.payload.Payload()
+}
+
+type vaultWalker struct {
+	graph *Graph
+	drv   Driver
+
+	materials []*material.Material
+
+	outputs map[*graph.Vertex][][]byte
+	skipped map[*graph.Vertex]bool
+}
+
+func (w *vaultWalker) lock(vrt *graph.Vertex) error {
+	node, err := w.graph.node(vrt)
+	if err != nil {
+		return err
+	}
+
+	id, err := node.Digest()
+	if err != nil {
+		return err
+	}
+
+	var mtrl *material.Material
+	switch node.Type() {
+	case CryptexNode:
+		cptx, err := node.Cryptex()
+		if err != nil {
+			return err
+		}
+
+		mtrl, err = w.lockCryptex(cptx, vrt)
+		if err != nil {
+			return err
+		}
+
+		return w.drv.StoreMaterial(mtrl)
+	case SecretNode:
+		if _, ok := w.outputs[vrt]; !ok {
+			panic("reached unprocessed non-cryptex node")
+		}
+
+		return nil
+	case MarkerNode:
+		data, ok := w.outputs[vrt]
+		if !ok {
+			panic("reached unprocessed non-cryptex node")
+		}
+
+		mtrl, err = material.New(id, data)
+		if err != nil {
+			return err
+		}
+
+		w.materials = append(w.materials, mtrl)
+		return nil
+	default:
+		return errors.New("unknown Node type")
+	}
+}
+
+func (w *vaultWalker) lockCryptex(cptx cryptex.Cryptex, vrt *graph.Vertex) (*material.Material, error) {
+	edges := w.graph.Edges(vrt)
+	inputs := make([][]byte, 0, w.graph.Adjacency[vrt].Len())
+
+	for _, vrt := range edges {
+		node, err := w.graph.node(vrt)
+		if err != nil {
+			return nil, err
+		}
+
+		switch node.Type() {
+		case CryptexNode, MarkerNode:
+			inputs = append(inputs, nil)
+		case SecretNode:
+			sec, err := node.Secret()
+			if err != nil {
+				return nil, err
+			}
+
+			data, skip := [][]byte{[]byte{}}, false
+			if sec.Phase() == secret.Dual {
+				if data, skip, err = w.drv.LoadSecret(sec); err != nil {
+					return nil, err
+				}
+			}
+			if skip {
+				w.skipped[vrt] = true
+			}
+
+			inputs = append(inputs, data...)
+		default:
+			return nil, errors.New("unknown Node type")
+		}
+
+	}
+
+	if err := cptx.Close(inputs, w.outputs[vrt]); err != nil {
+		return nil, err
+	}
+
+	for i, vrt := range edges {
+		w.outputs[vrt] = append(w.outputs[vrt], inputs[i])
+	}
+
+	node, err := w.graph.node(vrt)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := node.Digest()
+	if err != nil {
+		return nil, err
+	}
+
+	return material.New(id, w.outputs[vrt])
+}
+
+func (w *vaultWalker) shapeOutputs(vrt *graph.Vertex) error {
+	for _, v := range w.graph.Edges(vrt) {
+		w.outputs[v] = append(w.outputs[v], nil)
+	}
+	return nil
+}
+
+func (w *vaultWalker) unlock(vrt *graph.Vertex) error {
+	node, err := w.graph.node(vrt)
+	if err != nil {
+		return err
+	}
+
+	id, err := node.Digest()
+	if err != nil {
+		return err
+	}
+
+	mtrl, err := w.drv.LoadMaterial(id)
+	if err != nil {
+		return err
+	}
+	if mtrl != nil {
+		w.outputs[vrt] = mtrl.Data
+		return nil
+	}
+
+	switch node.Type() {
+	case CryptexNode:
+		cptx, err := node.Cryptex()
+		if err != nil {
+			return err
+		}
+		if mtrl, err = w.unlockCryptex(cptx, vrt); err != nil {
+			return err
+		}
+
+		if mtrl != nil {
+			return w.drv.StoreMaterial(mtrl)
+		}
+	case SecretNode:
+		sec, err := node.Secret()
+		if err != nil {
+			return err
+		}
+
+		output, skip, err := w.drv.LoadSecret(sec)
+		if err != nil {
+			return err
+		}
+		if skip {
+			w.skipped[vrt] = true
+		}
+		w.outputs[vrt] = output
+	case MarkerNode:
+		nfp, err := node.Digest()
+		if err != nil {
+			return err
+		}
+
+		mtrl, err := w.material(nfp)
+		if err != nil {
+			return err
+		}
+		w.outputs[vrt] = mtrl.Data
+	default:
+		return errors.New("unknown Node type")
+	}
+
+	return nil
+}
+
+func (w *vaultWalker) unlockCryptex(cptx cryptex.Cryptex, vrt *graph.Vertex) (*material.Material, error) {
+	edges := w.graph.Edges(vrt)
+	inputs := make([][]byte, 0, w.graph.Adjacency[vrt].Len())
+
+	skippable := false
+	for _, vrt := range edges {
+		inputs = append(inputs, w.outputs[vrt][0])
+		if len(w.outputs[vrt]) > 1 {
+			w.outputs[vrt] = w.outputs[vrt][1:]
+		} else {
+			w.outputs[vrt] = [][]byte{}
+		}
+
+		if w.skipped[vrt] {
+			skippable = true
+		}
+	}
+	if err := cptx.Open(w.outputs[vrt], inputs); err != nil {
+		if skippable {
+			w.skipped[vrt] = true
+			// TODO w.drv.Warn(err)
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	node, err := w.graph.node(vrt)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := node.Digest()
+	if err != nil {
+		return nil, err
+	}
+
+	return material.New(id, w.outputs[vrt])
+}
+
+func (w *vaultWalker) material(id []byte) (*material.Material, error) {
+	for _, mtrl := range w.materials {
+		if bytes.Equal(id, mtrl.ID) {
+			return mtrl, nil
+		}
+	}
+	return nil, errors.New("no Material for Node")
 }
--- vault_test.go
+++ vault_test.go
@@ -1,27 +1,157 @@
 package vcrypt
 
-import "testing"
+import (
+	"bytes"
+	"crypto/rand"
+	"io"
+	"testing"
+
+	"github.com/benburkert/vcrypt/internal/test"
+	"github.com/benburkert/vcrypt/secret"
+	"golang.org/x/crypto/openpgp"
+)
+
+var (
+	twoManVault, twoManSecret     = buildVault(twoManPlan, twoManDriver)
+	twoPartyVault, twoPartySecret = buildVault(twoPartyPlan, twoPartyDriver)
+	diamondVault, diamondSecret   = buildVault(diamondPlan, diamondDriver)
+	dnsSecVault, dnsSecSecret     = buildVault(dnsSecPlan, dnsSecDriver)
+
+	twoManDriver = test.Driver(map[string][]byte{
+		"op 1 secret": []byte("key #1"),
+		"op 2 secret": []byte("key #2"),
+	})
+
+	twoPartyDriver = test.Driver(map[string][]byte{
+		"party 1 password 2": []byte("step #3 secret"),
+		"party 2 password":   []byte("step #2 secret"),
+		"party 1 password 1": []byte("step #1 secret"),
+	})
+
+	diamondDriver = test.Driver(map[string][]byte{
+		"step 3 password":  []byte("step #3 password"),
+		"step 2a password": []byte("step #2a password"),
+		"step 2b password": []byte("step #2b password"),
+		"step 1 password":  []byte("step #1 password"),
+	})
+
+	dnsSecDriver = test.Driver(map[string][]byte{
+		test.OpenPGPKeys["alice"].KeyID:  mustOpenPGPKey(test.OpenPGPKeys["alice"].Private),
+		test.OpenPGPKeys["bob"].KeyID:    mustOpenPGPKey(test.OpenPGPKeys["bob"].Private),
+		test.OpenPGPKeys["claire"].KeyID: mustOpenPGPKey(test.OpenPGPKeys["claire"].Private),
+		test.OpenPGPKeys["david"].KeyID:  mustOpenPGPKey(test.OpenPGPKeys["david"].Private),
+		test.OpenPGPKeys["emily"].KeyID:  mustOpenPGPKey(test.OpenPGPKeys["emily"].Private),
+		test.OpenPGPKeys["frank"].KeyID:  mustOpenPGPKey(test.OpenPGPKeys["frank"].Private),
+		test.OpenPGPKeys["gloria"].KeyID: mustOpenPGPKey(test.OpenPGPKeys["gloria"].Private),
+	})
+)
 
 func TestVault(t *testing.T) {
 	tests := []struct {
-		*Plan
-
-		comment string
+		vault  *Vault
+		drv    Driver
+		secret []byte
 	}{
-		{
-			Plan:    twoManPlan,
-			comment: "two man rule",
-		},
+		{twoManVault, twoManDriver, twoManSecret},
+		{twoPartyVault, twoPartyDriver, twoPartySecret},
+		{diamondVault, diamondDriver, diamondSecret},
+		{dnsSecVault, dnsSecDriver, dnsSecSecret},
 	}
 
 	for _, test := range tests {
-		vault, err := NewVault(test.Plan, test.comment)
+		var got bytes.Buffer
+		if _, err := test.vault.Unlock(&got, test.drv); err != nil {
+			t.Error(err)
+			continue
+		}
+
+		want := test.secret
+		if !bytes.Equal(want, got.Bytes()) {
+			t.Errorf("vault unlocked bad secret: want %v, got %v", want, got.Bytes())
+		}
+	}
+}
+
+type skipDriver struct {
+	test.Driver
+
+	skippable map[string]bool
+}
+
+func (d skipDriver) LoadSecret(sec secret.Secret) ([][]byte, bool, error) {
+	if _, ok := d.Driver[sec.Comment()]; !ok {
+		return [][]byte{[]byte{}}, true, nil
+	}
+
+	return d.Driver.LoadSecret(sec)
+}
+
+func TestMultiPassVault(t *testing.T) {
+	vault := dnsSecVault
+	want, got := dnsSecSecret, bytes.Buffer{}
+
+	driver := skipDriver{
+		Driver: test.Driver{},
+	}
+
+	step := 0
+	for _, key := range test.OpenPGPKeys {
+		driver.Driver[key.KeyID] = mustOpenPGPKey(key.Private)
+
+		ok, err := vault.Unlock(&got, driver)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if fp, err := vault.Digest(); err == nil {
-			t.Errorf("unlocked vault has digest: %v", fp)
+		if step++; step == 5 {
+			if !ok {
+				t.Fatalf("vault failed to unlock after 5th pass")
+			}
+
+			break
 		}
+
+		if ok {
+			t.Fatalf("vault opened too early")
+		}
+		if len(got.Bytes()) > 0 {
+			t.Fatalf("vault unlock wrote secret prior to successful unlock")
+		}
+
+		delete(driver.Driver, key.KeyID)
 	}
+
+	if !bytes.Equal(want, got.Bytes()) {
+		t.Errorf("vault unlocked bad secret: want %v, got %v", want, got.Bytes())
+	}
+}
+
+func buildVault(plan *Plan, drv Driver) (*Vault, []byte) {
+	secret := make([]byte, 256)
+	if _, err := io.ReadFull(rand.Reader, secret); err != nil {
+		panic(err)
+	}
+
+	vault, err := NewVault(plan, plan.Comment())
+	if err != nil {
+		panic(err)
+	}
+
+	if err := vault.Lock(bytes.NewBuffer(secret), drv); err != nil {
+		panic(err)
+	}
+	return vault, secret
+}
+
+func mustOpenPGPKey(keyPEM string) []byte {
+	el, err := openpgp.ReadArmoredKeyRing(bytes.NewBufferString(keyPEM))
+	if err != nil {
+		panic(err)
+	}
+
+	buf := bytes.NewBuffer(nil)
+	if err := el[0].SerializePrivate(buf, nil); err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
 }
